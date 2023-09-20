Fix nested loops normalization
- updated test cases and make more verbose
- fix error as past version overwrote the body in normalization of loops

tests/test_analyse_dependency.py
@@ -6,10 +6,11 @@
 # nor does it submit to any jurisdiction.
 
 from pathlib import Path
+from shutil import rmtree
 import numpy as np
 import pytest
 from conftest import jit_compile
-from loki import Sourcefile
+from loki import Sourcefile, Loop, FindNodes, IntLiteral
 from loki.analyse.analyse_dependency_detection import normalize_bounds
 
 
@@ -18,13 +19,28 @@ def fixture_here():
     return Path(__file__).parent
 
 
-base_path = "sources/data_dependency_detection/"
+def is_normalized(node):
+    assert isinstance(node, Loop)
+    bounds = node.bounds
+    (a, _, c) = (bounds.start, bounds.stop, bounds.step)
 
-def test_fortran_result_consistency(here, test_file = "bounds_normalization.f90"):
+    if c is None:
+        c = IntLiteral(1)
+    return a == c == 1
+
+
+base_path = "sources/data_dependency_detection/"
+build_path = "build/data_dependency_detection/"
+
+
+def test_boundsnormalization(here, test_file="bounds_normalization.f90"):
+    rmtree(Path(here / build_path))
+    Path(here / build_path).mkdir(parents=True, exist_ok=True)
+
     original_filepath = here / base_path / test_file
 
     routine = Sourcefile.from_file(original_filepath)
-    filepath = here / "build/" / test_file.replace(".f90", "original_tmp.f90")
+    filepath = here / build_path / test_file.replace(".f90", "original_tmp.f90")
 
     function = jit_compile(
         routine, filepath=filepath, objname="boundsnormalizationtests"
@@ -37,7 +53,11 @@ def test_fortran_result_consistency(here, test_file = "bounds_normalization.f90"
     subroutine = routine.subroutines[0]
     subroutine.body = normalize_bounds(subroutine.body)
 
-    filepath = here / "build/" / test_file.replace(".f90", "normalized_tmp.f90")
+    loops = FindNodes(Loop).visit(subroutine.body)
+    for loop in loops:
+        assert is_normalized(loop)
+
+    filepath = here / build_path / test_file.replace(".f90", "normalized_tmp.f90")
 
     function = jit_compile(
         routine, filepath=filepath, objname="boundsnormalizationtests"
@@ -47,3 +67,49 @@ def test_fortran_result_consistency(here, test_file = "bounds_normalization.f90"
     function(n, actual)
 
     assert np.array_equal(expected, actual)
+
+
+different_value_initalization_functions = [
+    lambda dim1, dim2, dim3: np.ones(shape=(dim1, dim2, dim3), order="F"),
+    lambda dim1, dim2, dim3: np.zeros(shape=(dim1, dim2, dim3), order="F"),
+    np.random.rand,
+]
+
+
+@pytest.mark.parametrize(
+    "initialization_function", different_value_initalization_functions
+)
+def test_nested_loops_calculation(
+    here, initialization_function, test_file="bounds_normalization.f90"
+):
+    rmtree(Path(here / build_path))
+    Path(here / build_path).mkdir(parents=True, exist_ok=True)
+
+    original_filepath = here / base_path / test_file
+
+    routine = Sourcefile.from_file(original_filepath)
+    filepath = here / build_path / test_file.replace(".f90", "original_tmp.f90")
+
+    function = jit_compile(
+        routine, filepath=filepath, objname="nested_loops_calculation"
+    )
+
+    (dim1, dim2, dim3) = (10, 13, 12)
+    input_array = initialization_function(dim1, dim2, dim3)
+    expected = function(input_array, dim1, dim2, dim3)
+
+    subroutine = routine["nested_loops_calculation"]
+    subroutine.body = normalize_bounds(subroutine.body)
+
+    loops = FindNodes(Loop).visit(subroutine.body)
+    for loop in loops:
+        assert is_normalized(loop)
+
+    filepath = here / build_path / test_file.replace(".f90", "normalized_tmp.f90")
+
+    function = jit_compile(
+        routine, filepath=filepath, objname="nested_loops_calculation"
+    )
+    actual = function(input_array, dim1, dim2, dim3)
+
+    assert expected == actual