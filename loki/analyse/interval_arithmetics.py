34d98beeab4eca11542cc63bee6809a30a54a4dd
joscao <62660664+joscao@users.noreply.github.com>
2023-09-28 18:03:38 +0200

    Enable operations between RangeIndex and scalar types
    - provide addtional testing

--- loki/analyse/interval_arithmetics.py
+++ loki/analyse/interval_arithmetics.py
@@ -12,6 +12,7 @@ WARNING: Step size for ranges is disregarded in all operations, only valid upper
 https://en.wikipedia.org/wiki/Interval_arithmetic
 """
 
+from loki import RangeIndex
 
 __all___ = ["add", "sub", "mul", "binary_operation"]
 
@@ -20,41 +21,116 @@ def _get_bounds(x, y):
     return (x.start, x.stop, y.start, y.stop)
 
 
+def _binary_operation_on_range_index(op, x: RangeIndex, y: RangeIndex):
+    x_1, x_2, y_1, y_2 = _get_bounds(x, y)
+    tmp = (op(x_1, y_1), op(x_1, y_2), op(x_2, y_1), op(x_2, y_2))
+
+    return RangeIndex((min(tmp), max(tmp)))
+
+
+# both cases necessary since operation might not be symnetric
+def _binary_operation_on_range_index_and_scalar(op, x: RangeIndex, y):
+    x_1, x_2 = x.start, x.stop
+    tmp = (op(x_1, y), op(x_2, y))
+
+    return RangeIndex((min(tmp), max(tmp)))
+
+
+def _binary_operation_on_scalar_and_range_index(op, x, y: RangeIndex):
+    y_1, y_2 = y.start, y.stop
+    tmp = (op(x, y_1), op(x, y_2))
+
+    return RangeIndex((min(tmp), max(tmp)))
+
+
 def binary_operation(op, x, y):
     """
-    Apply binary operation `op` to intervals `x` and `y`. Assuming that the binary operation is defined for all `x`, `y`.
+    Apply binary operation `op` to intervals `x` and `y`. Assuming
+    that the binary operation is well defined.
     """
+    if x.__class__ == y.__class__ == RangeIndex:
+        return _binary_operation_on_range_index(op, x, y)
+
+    if x.__class__ is not RangeIndex:
+        return _binary_operation_on_scalar_and_range_index(op, x, y)
+
+    if y.__class__ is not RangeIndex:
+        return _binary_operation_on_range_index_and_scalar(op, x, y)
+
+
+def _add_on_range_index(x: RangeIndex, y: RangeIndex):
     x_1, x_2, y_1, y_2 = _get_bounds(x, y)
 
-    tmp = (op(x_1, y_1), op(x_1, y_2), op(x_2, y_1), op(x_2, y_2))
-    start = min(tmp)
-    stop = max(tmp)
+    return RangeIndex((x_1 + y_1, x_2 + y_2))
 
-    return x.__class__((start, stop))
+
+# single case sufficient since addition is symmetric
+def _add_on_range_index_and_scalar(x: RangeIndex, y):
+    x_1, x_2 = x.start, x.stop
+
+    return RangeIndex((x_1 + y, x_2 + y))
 
 
 def add(x, y):
+    if x.__class__ == y.__class__ == RangeIndex:
+        return _add_on_range_index(x, y)
+
+    if x.__class__ is not RangeIndex:
+        return _add_on_range_index_and_scalar(x=y, y=x)
+    if y.__class__ is not RangeIndex:
+        return _add_on_range_index_and_scalar(x, y)
+
+
+def _sub_on_range_index(x: RangeIndex, y: RangeIndex):
     x_1, x_2, y_1, y_2 = _get_bounds(x, y)
 
-    start = x_1 + y_1
-    stop = x_2 + y_2
+    return RangeIndex((x_1 - y_2, x_2 - y_1))
 
-    return x.__class__((start, stop))
+
+def _sub_on_range_index_and_scalar(x: RangeIndex, y):
+    x_1, x_2 = x.start, x.stop
+
+    return RangeIndex((x_1 - y, x_2 - y))
+
+
+def _sub_on_scalar_and_range_index(x, y: RangeIndex):
+    y_1, y_2 = y.start, y.stop
+
+    return RangeIndex((x - y_2, x - y_1))
 
 
 def sub(x, y):
+    if x.__class__ == y.__class__ == RangeIndex:
+        return _sub_on_range_index(x, y)
+
+    if x.__class__ is not RangeIndex:
+        return _sub_on_scalar_and_range_index(x, y)
+    if y.__class__ is not RangeIndex:
+        return _sub_on_range_index_and_scalar(x, y)
+
+
+def _mul_on_range_index(x: RangeIndex, y: RangeIndex):
     x_1, x_2, y_1, y_2 = _get_bounds(x, y)
 
-    start = x_1 - y_2
-    stop = x_2 - y_1
+    tmp = (x_1 * y_1, x_1 * y_2, x_2 * y_1, x_2 * y_2)
 
-    return x.__class__((start, stop))
+    return RangeIndex((min(tmp), max(tmp)))
+
+
+# multiplication is symmetric
+def _mul_on_range_index_and_scalar(x: RangeIndex, y):
+    x_1, x_2 = x.start, x.stop
+
+    tmp = (x_1 * y, x_2 * y)
+
+    return RangeIndex((min(tmp), max(tmp)))
 
 
 def mul(x, y):
-    x_1, x_2, y_1, y_2 = _get_bounds(x, y)
+    if x.__class__ == y.__class__ == RangeIndex:
+        return _mul_on_range_index(x, y)
 
-    start = min(x_1 * y_1, x_1 * y_2, x_2 * y_1, x_2 * y_2)
-    stop = max(x_1 * y_1, x_1 * y_2, x_2 * y_1, x_2 * y_2)
-
-    return x.__class__((start, stop))
+    if x.__class__ is not RangeIndex:
+        return _mul_on_range_index_and_scalar(x=y, y=x)
+    if y.__class__ is not RangeIndex:
+        return _mul_on_range_index_and_scalar(x, y)
--- tests/test_interval_arithmetics.py
+++ tests/test_interval_arithmetics.py
@@ -68,8 +68,18 @@ def test_mul(x, y, result):
         (lambda a, b: a + b, (1, 2), (3, 4), (4, 6)),  # Addition
         (lambda a, b: a - b, (5, 7), (2, 3), (2, 5)),  # Subtraction
         (lambda a, b: a * b, (-2, 2), (3, 3), (-6, 6)),  # Multiplication
-        (lambda a, b: a / b, (4, 8), (2, 2), (2.0, 4.0)),  # Division with casting to float
-        (lambda a, b: a // b, (3, 8), (2, 2), (1, 4)),  # Division with explicit integer division
+        (
+            lambda a, b: a / b,
+            (4, 8),
+            (2, 2),
+            (2.0, 4.0),
+        ),  # Division with casting to float
+        (
+            lambda a, b: a // b,
+            (3, 8),
+            (2, 2),
+            (1, 4),
+        ),  # Division with explicit integer division
         (lambda a, b: a**b, (2, 3), (2, 3), (4, 27)),  # Exponentiation
         (max, (1, 2), (3, 4), (3, 4)),  # Maximum
         (min, (5, 7), (2, 3), (2, 3)),  # Minimum
@@ -95,3 +105,39 @@ def test_custom_binary_operation(x, y, result):
     assert binary_operation(
         custom_operation, RangeIndex(x), RangeIndex(y)
     ) == RangeIndex(result)
+
+
+# test scalar cases
+@pytest.mark.parametrize(
+    "operation, optional_custom_op, input_x, input_y, expected_result",
+    [
+        (add, None, RangeIndex((1, 3)), 2, RangeIndex((3, 5))),
+        (add, None, 2, RangeIndex((1, 3)), RangeIndex((3, 5))),
+        (sub, None, RangeIndex((1, 3)), 2, RangeIndex((-1, 1))),
+        (sub, None, 2, RangeIndex((1, 3)), RangeIndex((-1, 1))),
+        (mul, None, RangeIndex((1, 3)), 2, RangeIndex((2, 6))),
+        (mul, None, 2, RangeIndex((1, 3)), RangeIndex((2, 6))),
+        (
+            binary_operation,
+            lambda x, y: x * y,
+            RangeIndex((1, 3)),
+            2,
+            RangeIndex((2, 6)),
+        ),
+        (
+            binary_operation,
+            lambda x, y: x * y,
+            2,
+            RangeIndex((1, 3)),
+            RangeIndex((2, 6)),
+        ),
+    ],
+)
+def test_operations_on_scalar_and_range_index(
+    operation, optional_custom_op, input_x, input_y, expected_result
+):
+    if optional_custom_op is None:
+        result = operation(input_x, input_y)
+    else:
+        result = operation(optional_custom_op, input_x, input_y)
+    assert result == expected_result
