b3dec3066077ad8c93021edceeae86b8ba67cccb
joscao <62660664+joscao@users.noreply.github.com>
2023-10-20 13:49:48 +0200

    Ensure order of i and j

--- tests/test_analyse_dependency.py
+++ tests/test_analyse_dependency.py
@@ -142,14 +142,18 @@ def test_nested_loops_calculation(
     ],
 )
 def test_access_function_creation(array_dimensions_expr, expected):
+    def unique_order_preserving(sequence):
+        seen = set()
+        return [x for x in sequence if not (x in seen or seen.add(x))]
+
     scope = Scope()
     first = parse_fparser_expression(f"z({array_dimensions_expr})", scope)
     make_all_variables_available = parse_fparser_expression("z(i,j)", scope)
 
     variables = FindVariables().visit(
-        (*first.dimensions, *make_all_variables_available.dimensions)
+        (*make_all_variables_available.dimensions, *first.dimensions)
     )
-    variables = list({str(var).lower() for var in variables})
+    variables = unique_order_preserving([str(var).lower() for var in variables])
 
     F, f, variables = construct_affine_array_access_function_representation(
         first.dimensions, variables
